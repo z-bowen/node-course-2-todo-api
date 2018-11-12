92ed75cb5ac9f752dd2838d078a5b69d4ad2c653
z-bowen <zach.bowen@gmail.com>
2018-11-12 18:08:44 +0100

    added GET /todos route

--- server/server.js
+++ server/server.js
@@ -23,6 +23,13 @@ app.post('/todos', (req, res) => {
 });
 
 // GET /todos
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+})
 
 app.listen(3000, () => {
   console.log(`Server listening on port ${port}`);
