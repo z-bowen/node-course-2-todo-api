bfd261e8357afe40c0e03813a8ada28cb46ed70a
z-bowen <zach.bowen@gmail.com>
2018-11-14 13:42:28 +0100

    Added POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -65,6 +65,25 @@ UserSchema.statics.findByToken = function (token) {
 
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+  return User.findOne({
+    email
+  }).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, valid) => {
+        if (valid) {
+          return resolve(user);
+        }
+        return reject(err);
+      });
+    });
+  });
+};
+
 UserSchema.pre('save', function (next) {
   var user = this;
 
--- server/server.js
+++ server/server.js
@@ -127,8 +127,21 @@ app.post('/users', (req, res) => {
   });
 });
 
+// GET /users/me
 app.get('/users/me', authenticate, (req, res) => {
   res.send(res.user);
+});
+
+// POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password).then((user) => {
+      return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);
+      });
+  }).catch((e) => {
+    return res.status(400).send({error: "Invalid email or password"});
+  });
 });
 
 app.listen(port, () => {
